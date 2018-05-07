8e5f59448940761e2c11caea3ab262d585241358
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-05-07 19:28:09 -0400

    Everything is workinggit statusgit statusgit statusgit status! 20 recent posts from other usersgit status! And 20 recent posts from user! :)

--- models.rb
+++ models.rb
@@ -2,11 +2,6 @@ class User < ActiveRecord::Base
     has_many :posts
 end
 
-# class Profile < ActiveRecord::Base
-#     has_many :posts
-#     belongs_to :user
-# end
-
 class Post < ActiveRecord::Base
     belongs_to :user
 end
